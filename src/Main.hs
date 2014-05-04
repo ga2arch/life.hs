44dada8eabe63debd0a724c292d24ad7a6782cf0
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-05-04 13:28:53 +0200

    playing with persistent and esqueleto

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,44 @@
 {-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies #-}
-{-# LANGUAGE OverloadedStrings, GADTs, FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings, GADTs, FlexibleContexts, ScopedTypeVariables #-}
 
 module Main where
 
+import Control.Applicative
+import Control.Monad.IO.Class (liftIO)
+import Data.Time
 import Data.Text
-import Database.Persist
-import Database.Persist.Sqlite
+import Database.Esqueleto
+import Database.Persist.Sqlite (runSqlite, runMigrationSilent)
 import Database.Persist.TH
 
+share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
+Commit
+    content  Text
+    date     UTCTime
+    deriving Show
+
+Tag
+    name    Text
+    commit  CommitId
+    deriving Show
+|]
+
 main :: IO ()
-main = undefined
+main = runSqlite ":memory:" $ do 
+    buildDb
+
+    commits <- select $ from $ \(c :: SqlExpr (Entity Commit)) -> do 
+        return c
+
+    tags <- select $ from $ \(c, t) -> do
+        where_ (t ^. TagCommit ==. c ^. CommitId)
+        return t
+
+    liftIO $ print tags
     
+
+buildDb = do 
+    today <- liftIO $ getCurrentTime
+    runMigrationSilent migrateTables
+    commit <- insert $ Commit "Went to the @gym" today
+    insert $ Tag "gym" commit
