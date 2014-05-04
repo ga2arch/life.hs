d5dbe859c442ad01598df2247ab5e379e97450da
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-05-04 17:46:32 +0200

    better visualization

--- src/Main.hs
+++ src/Main.hs
@@ -6,12 +6,13 @@ module Main where
 import Control.Applicative
 import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.Time
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, unpack)
 import Data.List (foldl')
 import Data.List.Split
 import Database.Esqueleto
 import Database.Persist.Sqlite (runSqlite, runMigrationSilent)
 import Database.Persist.TH
+import Text.PrettyPrint.ANSI.Leijen
 import System.Environment
 
 import qualified Data.Map as M
@@ -32,44 +33,43 @@ Tag
 |]
 
 main :: IO ()
-main = do
-    runSqlite "life.db" $ runMigrationSilent migrateTables
+main = runSqlite "life.db" $ do
+    runMigrationSilent migrateTables
 
-    (cmd:args) <- getArgs
+    (cmd:args) <- liftIO getArgs
 
     --let cmd = "show"
     --let args = []
     case cmd of 
         "commit" -> commitCmd args
         "show"   -> showCmd args
-        _        -> putStrLn "no implemented"
+        _        -> liftIO $ putStrLn "no implemented"
 
---commit :: [String] -> IO ()
-commitCmd (content:_) = runSqlite "life.db" $ do
+commitCmd (content:_) = do
     now <- liftIO $ getCurrentTime
     commitId <- insert $ Commit (pack content) now (utctDay now)
     mapM_ insert $ getTags commitId content
 
-showCmd [] = runSqlite "life.db" $ do
-
-    commits <- select 
-        $ from 
+showCmd [] = do
+    commits <- select $ from 
         $ \(c :: SqlExpr (Entity Commit)) -> do 
             return c
 
     let d = foldl' f M.empty commits
-
-    --mapM_ pp $ M.toList d
-    liftIO $ print d
+    mapM_ pp $ M.toList d
   where
     f m c = do
-        let commit@(Commit content date day) = entityVal c
-        M.insertWithKey u day [content] m
+        let (Commit content date day) = entityVal c
+        M.insertWithKey u day [text.unpack $ content] m
     u _ nv ov = ov ++ nv
-
-    --pp = 
+    pp (day, commits) = do 
+        liftIO $ putDoc 
+                $ (text $ show day) 
+                <$$> (indent 1 $ vsep (map ((<+>) (dot <> space)) commits))
+                <$$> linebreak 
 
 getTags :: CommitId -> String -> [Tag]
 getTags commitId content = do
     map (\name -> Tag (pack name) commitId) $ 
         split (startsWith "@") content
+
