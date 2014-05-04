f47aee671c8720ea5688a14fbcb5774f04e0bbe8
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-05-04 17:26:51 +0200

    added showCmd

--- src/Main.hs
+++ src/Main.hs
@@ -4,19 +4,25 @@
 module Main where
 
 import Control.Applicative
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.Time
 import Data.Text (Text, pack)
+import Data.List (foldl')
 import Data.List.Split
 import Database.Esqueleto
 import Database.Persist.Sqlite (runSqlite, runMigrationSilent)
 import Database.Persist.TH
 import System.Environment
 
+import qualified Data.Map as M
+
+--import Types
+
 share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
 Commit
     content  Text
     date     UTCTime
+    day      Day
     deriving Show
 
 Tag
@@ -27,39 +33,43 @@ Tag
 
 main :: IO ()
 main = do
+    runSqlite "life.db" $ runMigrationSilent migrateTables
+
     (cmd:args) <- getArgs
+
+    --let cmd = "show"
+    --let args = []
     case cmd of 
-        "commit" -> runSqlite "life.db" $ commit args
+        "commit" -> commitCmd args
+        "show"   -> showCmd args
         _        -> putStrLn "no implemented"
 
 --commit :: [String] -> IO ()
-commit (content:_) = do
-    today <- liftIO $ getCurrentTime
-    commitId <- insert $ Commit (pack content) today
-
+commitCmd (content:_) = runSqlite "life.db" $ do
+    now <- liftIO $ getCurrentTime
+    commitId <- insert $ Commit (pack content) now (utctDay now)
     mapM_ insert $ getTags commitId content
+
+showCmd [] = runSqlite "life.db" $ do
+
+    commits <- select 
+        $ from 
+        $ \(c :: SqlExpr (Entity Commit)) -> do 
+            return c
+
+    let d = foldl' f M.empty commits
+
+    --mapM_ pp $ M.toList d
+    liftIO $ print d
+  where
+    f m c = do
+        let commit@(Commit content date day) = entityVal c
+        M.insertWithKey u day [content] m
+    u _ nv ov = ov ++ nv
+
+    --pp = 
 
 getTags :: CommitId -> String -> [Tag]
 getTags commitId content = do
     map (\name -> Tag (pack name) commitId) $ 
         split (startsWith "@") content
-
-{--main :: IO ()
-main = runSqlite "life.db" $ do 
-    buildDb
-
-    commits <- select $ from $ \(c :: SqlExpr (Entity Commit)) -> do 
-        return c
-
-    tags <- select $ from $ \(c, t) -> do
-        where_ (t ^. TagCommit ==. c ^. CommitId)
-        return t
-
-    liftIO $ print tags
-    
-
-buildDb = do 
-    --today <- liftIO $ getCurrentTime
-    runMigrationSilent migrateTables
-    --commit <- insert $ Commit "Went to the @gym" today
-    --insert $ Tag "gym" commit--}
