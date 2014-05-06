0d24c661a035769d58cdf8df0245f2c788e3584f
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-05-06 20:39:25 +0200

    moved types to Types.hs

---
+++ src/Types.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, DeriveDataTypeable#-}
+{-# LANGUAGE OverloadedStrings, GADTs, FlexibleContexts, ScopedTypeVariables #-}
+
+module Types where
+
+import Data.Data
+import Data.Text (Text)
+import Data.Time
+import Data.Typeable
+import Database.Persist.TH
+
+data Command = Command { 
+    cmd :: String 
+,   commandArgs :: [String]
+} deriving (Show, Data, Typeable)
+
+--  SQL 
+
+share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
+Commit
+    content  Text
+    date     UTCTime
+    day      Day
+    deriving Show
+
+Tag
+    name    Text
+    commit  CommitId
+    deriving Show
+|]
+
+
