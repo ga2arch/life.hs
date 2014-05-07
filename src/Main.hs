c510d32b2009116e2c52c1f254adf8c3dc73375f
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-05-07 10:59:49 +0200

    fixed bug with getTags

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.Time
 import Data.Text (Text, pack, unpack)
 import Data.List (foldl', reverse)
-import Data.List.Split
+import Data.String.Utils
 import Database.Esqueleto
 import Database.Persist.Sqlite (runSqlite, runMigrationSilent)
 import Text.PrettyPrint.ANSI.Leijen
@@ -28,13 +28,14 @@ main = runSqlite database $ do
         cmd = def &= argPos 0 &= opt ("show" :: String)
     ,   commandArgs = def &= args
     }
+
     case cmd of 
         "commit" -> commitCmd commandArgs >> showCmd []
         "show"   -> showCmd commandArgs
         "delete" -> deleteCmd commandArgs >> showCmd []
+        "tags"   -> tagsCmd
         _        -> liftIO $ putStrLn "not implemented"
   
-
 groupCommitsByDate = foldl' f M.empty
   where
     f m c = do
@@ -47,16 +48,22 @@ allCommits = select $
     from $ \(c :: SqlExpr (Entity Commit)) -> do
         return c
 
+allTags = select $ 
+    from $ \(t :: SqlExpr (Entity Tag)) -> do
+        return t
+
 commitCmd (content:_) = do
     now <- liftIO $ getCurrentTime
     commitId <- insert $ Commit (pack content) now (utctDay now)
-    mapM_ insert $ getTags commitId content
+    let tags = getTags commitId content
+    liftIO $ print tags
+    mapM_ insert tags
 
 showCmd _ = do
     d <- fmap groupCommitsByDate allCommits
     mapM_ pp $ M.toDescList d
   where
-    pp (day, commits) = liftIO $ putDoc $ mkDoc day commits
+    pp (day, commits) = liftIO . putDoc $ mkDoc day commits
     mkDoc day commits = (text $ show day) 
         <$$> indent 1 (vsep $ map (((<+>) $ dot <> space).snd) commits)
         <$$> linebreak 
@@ -69,8 +76,20 @@ deleteCmd (d:pos:_) = do
     delete $ from $ \c -> do
         where_ (c ^. CommitId ==. val k)
 
+tagsCmd = do
+    t <- allTags
+    liftIO . putDoc . mkDoc $ map (text.unpack.tagName.entityVal) t
+  where
+    mkDoc tags = do
+        (text "Tags")
+        <>   linebreak
+        <$$> vsep tags
+        <>   linebreak
+
 getTags :: CommitId -> String -> [Tag]
 getTags commitId content = do
-    map (\name -> Tag (pack name) commitId) $ 
-        split (startsWith "@") content
+    filterMap (startswith "@") ((flip Tag) commitId . pack) $ splitWs content
+  where
+    filterMap fe fm xs = foldl' (u fe fm) [] xs
+    u fe fm acc e = if fe e then acc ++ [fm e] else acc
 
